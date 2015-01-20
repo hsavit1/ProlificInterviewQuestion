e2b4f8f75a4555e806beeb9c4bdfb4ee817ca3fb
Henry Savit <henrysavit@Henrys-MBP.home>
2015-01-20 13:37:30 -0500

    Initial Commit

---
+++ ProlificInterviewQuestion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProlificInterviewQuestion
+//
+//  Created by Henry Savit on 1/20/15.
+//  Copyright (c) 2015 Henry Savit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
